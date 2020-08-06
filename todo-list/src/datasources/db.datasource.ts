eeed1fa49c4c73bdc98cc93dec12d651db80cebd
Kayra <KayraAlat@gmail.com>
2020-08-06 14:37:00 +0900

    Fixed datasource and played around with it a bit

--- todo-list/src/datasources/db.datasource.ts
+++ todo-list/src/datasources/db.datasource.ts
@@ -1,11 +1,11 @@
-import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
-import {juggler} from '@loopback/repository';
+import { inject, lifeCycleObserver, LifeCycleObserver } from '@loopback/core';
+import { juggler } from '@loopback/repository';
 
 const config = {
   name: 'db',
   connector: 'memory',
   localStorage: '',
-  file: './data/db.json'
+  file: './data/db.json',
 };
 
 // Observe application's life cycle to disconnect the datasource when
@@ -13,13 +13,14 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class DbDataSource extends juggler.DataSource
+export class DbDataSource
+  extends juggler.DataSource
   implements LifeCycleObserver {
   static dataSourceName = 'db';
   static readonly defaultConfig = config;
 
   constructor(
-    @inject('datasources.config.db', {optional: true})
+    @inject('datasources.config.db', { optional: true })
     dsConfig: object = config,
   ) {
     super(dsConfig);
