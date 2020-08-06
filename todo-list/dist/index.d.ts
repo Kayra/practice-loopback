1dd927dcffa224bb12958e42d6396dbbba6ddbe4
Kayra <KayraAlat@gmail.com>
2020-08-06 14:19:00 +0900

    Removed dist

--- todo-list/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, TodoListApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<TodoListApplication>;
